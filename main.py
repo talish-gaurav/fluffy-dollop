4a89d5da462deaed79ef7b8170a55dc871b69ea2
talish-gaurav <97028189+talish-gaurav@users.noreply.github.com>
2024-12-07 07:14:47 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+
+def fact(n):
+    
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n*fact(n-1)
+    
+n = int(input("Enter any INTEGER"))
+        
+print("factorial of",n,"is",fact(n))
